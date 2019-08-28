9b60a62dc2f13f47bba0eab34fdb07db716751d2
erikschmutz <erik.rehn98@gmail.com>
2019-08-28 16:19:09 +0200

    init

---
+++ dist/.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+const buttonPressesLogFile = "./src/index.c";
+const template = require("./template.js");
+console.log(`Watching for file changes on ${buttonPressesLogFile}`);
+
+update();
+fs.watchFile(buttonPressesLogFile, { interval: 100 }, update);
+
+function update(err, data) {
+  exec(
+    "gcc ./src/index.c -o ./dist/.out && ./dist/.out",
+    (err, stdout, stderr) => {
+      if (err || stderr) {
+        createPage("<div style='color:red'>" + err + "</div>");
+        return;
+      }
+      createPage(stdout);
+    }
+  );
+}
+
+function createPage(input) {
+  input = input.replace(/\n/g, "<br>");
+  console.log(input);
+  console.log(input);
+  fs.writeFileSync("./dist/index.html", template(input));
+}
---
+++ src/index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("Thanks for using C-LIVE the interactive c-live reloader. We support all native <b>html tags</b> and uses the react native css to make sure everyone is comfortable with the style of the website\n");
+    printf("\n");
+    printf("To check run program simple install with <p style='color:green'> $ npm install </p> then simple run by  running <p style='color:green'>$ npm start </p>");
+
+    printf("<mark>Happy hacking!</mark>\n");
+    printf(" ");
+  return 0;
+}
